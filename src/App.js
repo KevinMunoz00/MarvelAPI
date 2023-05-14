bf6f9e7a4862262cb5bd5ba0a99bf3e4fbbbb2be
UCC_AMS_2310_Equipo03 <johan.perezt@campusucc.edu.co>
2023-05-13 20:37:06 -0500

    ymlstories

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import marvelSub from "./assets/marvel-sub.png";
 import searchLogo from "./assets/search.png";
 
 function App() {
-  const [stories, setStories] = useState([]);
+  const [stories, setstories] = useState([]);
   useEffect(() => {
-    axios.get('https://rm4afqiyjeisqizhyarhngfole0lyulk.lambda-url.us-east-1.on.aws/')
+    axios.get('https://qxvow6mohts7fdzaut7by5jqpy0gtrdq.lambda-url.us-east-1.on.aws/')
       .then(response => {
-        setStories(response.data.data.results);
-        console.log(response.data.data.results)
+        setstories(response.data.data.results);
       })
       .catch(error => {
         console.log(error);
@@ -20,50 +19,44 @@ function App() {
   }, []);
 
   return (
-    <div className="container-fluid px-0">
-      <div className="container-fluid px-0">
-        <div className="header">
-          <div id='elem'>
-            <img id='logousu' src={logoUsuario} alt="Logo Usuario" className="img-fluid" />
-            <h1 id='text1' className="d-inline-block ml-2 mb-0 text-white">FAMILIA</h1>
-          </div>
-          <div id='elem' className='elem1'>
-            <img src={marvelLogo} alt="Marvel Logo" className="img-fluid" />
-          </div>
-          <div id='elem'>
-            <img src={marvelSub} alt="Marvel Unlimited Subscribe" className="img-fluid" />
-            <h1 id='text2' className="d-inline-block ml-10 mb-0 text-white">MARVEL UNLIMITED SUBSCRIBE</h1>
-          </div>
-          <div id='elem'>
-            <img id='searchl' src={searchLogo} alt="Search Logo" className="img-fluid" />
-          </div>
+    <div className="container-fluid">
+      <div className='contenedor-iconos'>
+        <div id='elem'>
+          <img className='logo-usu' src={logoUsuario} />
+          <h1 className='texto-arriba'>FAMILIA</h1>
         </div>
-
-        <nav className="navbar navbar-expand-md navbar-dark bg-marvel py-0">
-          <div className="container">
-            <div className="" id="navbarNav">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#">CHARACTERS</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">COMICS</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">SERIES</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">STORIES</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <div id='elem' className='elem2'>
+          <img className='logo-marvel' src={marvelLogo} />
+        </div>
+        <div id='elem'>
+          <img className='logo-marvel-sub' src={marvelSub} />
+          <h1 className="texto-arriba">MARVEL UNLIMITED SUBSCRIBE</h1>
+        </div>
+        <div id='elem'>
+          <img id='searchl' src={searchLogo} alt="Search Logo" className="img-fluid" />
+        </div>
       </div>
 
-      <h1 className="text-center mt-5 mb-5">Marvel Stories</h1>
+      <nav className='contenedor-menu'>
+        <ul className='menu'>
+          <li className='menu-celdas'>
+            <a className='menu-enlaces' href='#'>PERSONAJES</a>
+          </li>
+          <li className='menu-celdas'>
+            <a className='menu-enlaces' href='#'>COMICS</a>
+          </li>
+          <li className='menu-celdas'>
+            <a className='menu-enlaces' href='#'>SERIES</a>
+          </li>
+          <li className='menu-celdas'>
+            <a className='menu-enlaces' href='#'>HISTORIAS</a>
+          </li>
+        </ul>
+      </nav>
+
+
+      <h1 className="titulo">Marvel Stories</h1>
       <div class="container">
-        <h1>Stories</h1>
         <table class="table">
           <thead>
             <tr>
@@ -89,7 +82,7 @@ function App() {
           </tbody>
         </table>
       </div>
-    </div >
+    </div>
   );
 }
 
