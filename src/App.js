f654faf0875fd2f21b14768cd03b925cc5eb40c7
UCC_AMS_2310_Equipo03 <kevin.munoz@campusucc.edu.co>
2023-05-17 14:45:50 -0500

    Prueba Link marvelcomic to maverlcharacters

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
       <nav className='contenedor-menu'>
         <ul className='menu'>
           <li className='menu-celdas'>
-            <a className='menu-enlaces' href='https://marvelcharactersv2.s3.amazonaws.com/index.html'>PERSONAJES</a>
+            <a className='menu-enlaces' href='https://d3fv4a5rs1kzo7.cloudfront.net/characters/index.html'>PERSONAJES</a>
           </li>
           <li className='menu-celdas'>
             <a className='menu-enlaces' href='https://marvelcomicsv2.s3.amazonaws.com/index.html'>COMICS</a>
