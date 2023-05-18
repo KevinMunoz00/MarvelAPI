3583de1276dee63042e41af037abf11aecb2f7b2
UCC_AMS_2310_Equipo03 <johan.perezt@campusucc.edu.co>
2023-05-17 19:06:20 -0500

    cloudfrontseries

--- src/App.js
+++ src/App.js
@@ -36,24 +36,24 @@ function App() {
           <img id='searchl' src={searchLogo} alt="Search Logo" className="img-fluid" />
         </div>
       </div>
-      
+
       <nav className='contenedor-menu'>
         <ul className='menu'>
           <li className='menu-celdas'>
-            <a className='menu-enlaces' href='https://marvelcharactersv2.s3.amazonaws.com/index.html'>PERSONAJES</a>
+            <a className='menu-enlaces' href='https://d3fv4a5rs1kzo7.cloudfront.net/characters/index.html'>PERSONAJES</a>
           </li>
           <li className='menu-celdas'>
-            <a className='menu-enlaces' href='https://marvelcomicsv2.s3.amazonaws.com/index.html'>COMICS</a>
+            <a className='menu-enlaces' href='https://d3fv4a5rs1kzo7.cloudfront.net'>COMICS</a>
           </li>
           <li className='menu-celdas'>
-            <a className='menu-enlaces' href='https://marvelseries1.s3.amazonaws.com/index.html'>SERIES</a>
+            <a className='menu-enlaces' href='https://d3fv4a5rs1kzo7.cloudfront.net/series/index.html'>SERIES</a>
           </li>
           <li className='menu-celdas'>
-            <a className='menu-enlaces' href='https://marvelstories1.s3.amazonaws.com/index.html'>HISTORIAS</a>
+            <a className='menu-enlaces' href='https://d3fv4a5rs1kzo7.cloudfront.net/stories/index.html'>HISTORIAS</a>
           </li>
         </ul>
       </nav>
-
+      
       <h1 className="titulo">Marvel Series</h1>
       <div className='contenedor-series'>
         {series.map((serie) => (
